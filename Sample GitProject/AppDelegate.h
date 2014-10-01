bdbb6893c6fb5dca9c47c0b12348758f3c3982e6
ImpMobiledev <lokesh.natarajan@impigertech.com>
2014-10-01 13:24:06 +0530

    Initial Commit

---
+++ Sample GitProject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Sample GitProject
+//
+//  Created by Rajkumar Sakthivel on 01/10/14.
+//  Copyright (c) 2014 Impiger Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Sample GitProject/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Sample GitProject
+//
+//  Created by Rajkumar Sakthivel on 01/10/14.
+//  Copyright (c) 2014 Impiger Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
